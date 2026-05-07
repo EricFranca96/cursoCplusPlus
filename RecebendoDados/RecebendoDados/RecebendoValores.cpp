75e329c983998135ca7a0a4ee3c62b56b21076e6
EricFranca96 <ericprogramacao96@gmail.com>
2026-05-07 08:00:15 -0300

    Adiciona .gitignore e ignora arquivos do Visual Studio

---
+++ RecebendoDados/RecebendoDados/RecebendoValores.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+int main()
+{
+	setlocale(LC_ALL, "pt_BR.UTF-8");
+	// setlocale(LC_ALL, "pt_BR.UTF-8"); Para poder usar acentos e caracteres especiais do português-BR
+	int Numero01, Numero02;
+	std::cout << "Digite o primeiro número: ";
+	std::cin >> Numero01;
+	std::cout << "Numero01 = " << Numero01 << "\n";
+	std::cout << "Digite o segundo número: ";
+	std::cin >> Numero02;
+	std::cout << "Numero02 = " << Numero02 << "\n";
+
+
+
+
+	std::cout << "\n Precione Enter para sair...\n";
+	std::cin.get();
+	return 0;
+}
