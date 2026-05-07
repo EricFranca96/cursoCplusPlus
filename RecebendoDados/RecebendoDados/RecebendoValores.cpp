33e94451572f1484f324e9ccf3c4fe77e94e681a
EricFranca96 <ericprogramacao96@gmail.com>
2026-05-07 08:21:43 -0300

    Aprendendo óperadores aritméticos C++

--- RecebendoDados/RecebendoDados/RecebendoValores.cpp
+++ RecebendoDados/RecebendoDados/RecebendoValores.cpp
@@ -5,12 +5,15 @@ int main()
 	setlocale(LC_ALL, "pt_BR.UTF-8");
 	// setlocale(LC_ALL, "pt_BR.UTF-8"); Para poder usar acentos e caracteres especiais do português-BR
 	int Numero01, Numero02;
+	int Resultado;
 	std::cout << "Digite o primeiro número: ";
 	std::cin >> Numero01;
 	std::cout << "Numero01 = " << Numero01 << "\n";
 	std::cout << "Digite o segundo número: ";
 	std::cin >> Numero02;
 	std::cout << "Numero02 = " << Numero02 << "\n";
+	Resultado = Numero01 + Numero02;
+	std::cout << "A soma dos dois numeros: " << Resultado << "\n";
 
 
 
